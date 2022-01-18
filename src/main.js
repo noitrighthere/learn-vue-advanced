d23aa01ed2d59eb784928b04ee6a9c0f9067a672
nokh9503 <nokh9503@hanmail.net>
2022-01-18 23:24:23 +0900

    create new project

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { router } from './routes/index';
+
+/* main.js는 기본적으로 애플리케이션의 설정들을 파악할 수 있는 청사진임 */
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router,
+}).$mount('#app')
---
+++ src/routes/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView'
+import AskView from '../views/AskView'
+import JobsView from '../views/JobsView'
+import UserView from '../views/UserView'
+import ItemView from '../views/ItemView'
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  // #(해시)값을 없앰
+  mode: 'history',
+  // 라우터에 대한 정보를 관리하는 객체
+  routes: [
+    {
+      path: '/',
+      redirect: '/news',
+    },
+    {
+      // path : url 주소
+      path: '/news',
+      // component : url 주소로 갔을 때 표시될 컴포넌트 (페이지)
+      component: NewsView,
+    },
+    {
+      path: '/ask',
+      component: AskView,
+    },
+    {
+      path: '/jobs',
+      component: JobsView,
+    },
+    {
+      path: '/user',
+      component: UserView,
+    },
+    {
+      path: '/item',
+      component: ItemView,
+    },
+  ]
+});
