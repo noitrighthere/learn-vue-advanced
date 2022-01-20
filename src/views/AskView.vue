c41283fd4d707248fc8c8189a0901a3e91e2eb13
nokh9503 <nokh9503@hanmail.net>
2022-01-20 10:55:44 +0900

    api 구현

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-    ask
+    <div v-for="(item, index) in ask" :key="index">{{ item.title }}</div>
   </div>
 </template>
 
 <script>
-export default {
+import { fetchAskList } from '../api/index.js';
 
+export default {
+  data() {
+    return {
+      ask: []
+    }
+  },
+  // 컴포넌트가 생성되자마자 실행되는 로직(life cycle hook)
+  created() {
+    var vm = this;
+    fetchAskList()
+      .then(function(response) {
+        vm.ask = response.data;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  },
+  
 }
 </script>
 
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div>{{ user }}</div>
+    <div v-for="(user, index) in users" :key="index">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import { fetchNewsList } from '../api/index.js';
 
 export default {
   data() {
@@ -14,15 +14,14 @@ export default {
     }
   },
   created() {
-    var vm = this;
-    axios.get('https://api.hnpwa.com/v0/news/1.json')
-      .then(function(response) {
-        console.log(response);
-        vm.users = response.data;
+    fetchNewsList()
+      .then(response => {
+        console.log(this);
+        this.users = response.data;
       })
       .catch(function(error) {
         console.log(error);
-      })
+      });
   },
 }
 </script>
